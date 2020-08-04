45812a8f061adb6ab10cbdb46e87d4e5d108ee2c
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-08-04 12:47:31 -0400

    ! Trabajando con la validacion del formulario de solicitud de ciudad

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+console.log("El Viajero - Bienvenido!");
 // FORM
 const formNewCity = document.getElementById("form-new-city");
 // INPUT
@@ -6,50 +7,57 @@ const emailInput = document.getElementById("email-input");
 // HELPS
 const ciudadHelp = document.getElementById("ciudad-help");
 const emailHelp = document.getElementById("email-help");
+// BUTTON
+const btnRegisterCity = document.getElementById("btn-register-city");
 
-console.log(formNewCity);
+// Event Listeners: Form
+formNewCity.addEventListener("submit", sendRequestCiudad);
+// Event Listeners: Inputs
+ciudadInput.addEventListener("change", (event) => checkCiudad())
+emailInput.addEventListener("change", (event) => checkEmail())
 
-// Event
-formNewCity.addEventListener("submit", (event) => {
+function sendRequestCiudad(event) {
     // Cancel the default behavior :)
     event.preventDefault();
-
     // Values
+    const isCiudadOK = checkCiudad();
+    const isEmailOK = checkEmail();
+
+    if (isCiudadOK && isEmailOK) {
+        btnRegisterCity.classList.remove("disable");
+        btnRegisterCity.removeAttribute("disabled");
+        alert("Datos registrados!, gracias.");
+    } else {
+        btnRegisterCity.classList.add("disable");
+        btnRegisterCity.setAttribute("disabled", true);
+    }
+}
+
+function checkCiudad() {
+    let isOKCiudad = false;
     const ciudadValue = ciudadInput.value;
-    const emailValue = emailInput.value;
-    if (ciudadValue === "" || ciudadValue === undefined || ciudadValue === null) {
+    const isValidCiudad = ciudadValue === "" || ciudadValue === undefined || ciudadValue === null;
+    if (isValidCiudad) {
         ciudadHelp.classList.remove("d-none");
+        isOKCiudad = true;
     } else {
         ciudadHelp.classList.add("d-none");
     }
-    if (emailValue === "" || emailValue === undefined || emailValue === null) {
+    return isOKCiudad;
+}
+
+function checkEmail() {
+    let isOKEmail = false;
+    const emailValue = emailInput.value;
+    const isValidEmail = emailValue === "" || emailValue === undefined || emailValue === null;
+    if (isValidEmail) {
         emailHelp.classList.remove("d-none");
+        isOKEmail = true;
     } else {
         emailHelp.classList.add("d-none");
     }
-
-    alert("Datos registrados!, gracias.");
-});
-
-
-
-ciudadHelp.addEventListener("keyup", (event) => {
-    console.log("ciudadHelp", ciudadHelp);
-    ciudadHelp.classList.add("d-none");
-})
-
-emailHelp.addEventListener("keyup", (event) => {
-    emailHelp.classList.add("d-none");
-})
-
-
-
-// if (emailValue === "" || emailValue === undefined || emailValue === null) {
-//     emailHelp.classList.remove("d-none");
-// }
-
-// console.log(ciudadValue);
-// console.log(emailValue);
+    return isOKEmail;
+}
 
 // JQuery
 $('.collapse').collapse();
